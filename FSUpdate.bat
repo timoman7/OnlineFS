88674fc51b88c4741fb269741bb8e789161ec6ba
Tim C <timoman8@hotmail.com>
2018-04-06 11:58:28 -0400

    Update

---
+++ FSUpdate.bat
@@ -0,0 +1,1 @@
+call UpdateThroughFS.bat .\ %cd%
--- UpdateThroughFS.bat
+++ UpdateThroughFS.bat
@@ -2,9 +2,11 @@
 set _home_=%2
 echo Working from: %_home_%
 echo Writing in: %1
+echo Known as: %cd%
 (((dir /A:D /B | find /V ".prn") | find /V "_EXCLUDE") | find /V ".git") > folders.prn
 (dir /A:-D /B | find /V ".prn") > files.prn
 for /d %%i in (%1*) do (
+echo Found: %%i
 cd %%i
 call %_home_%\UpdateThroughFS.bat .\ %_home_%
 cd %_home_%
